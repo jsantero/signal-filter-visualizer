6ba0cf346a9cdb7ef8b630d518d16a9b56d5a409
Juha-Matti Santero <juha.santero@gmail.com>
2016-11-15 20:02:55 +0200

    Added menu bar.

--- signal_filter_visualizer.py
+++ signal_filter_visualizer.py
@@ -15,6 +15,26 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(_mainWidget)
         self.setWindowTitle("Signal Filter Visualizer")
 
+        fileOpenAction = QAction("&Open", self)
+        fileOpenAction.setShortcut(QKeySequence.Open)
+        helpText = "Import samples from a file"
+        fileOpenAction.setToolTip(helpText)
+        fileOpenAction.setStatusTip(helpText)
+        fileOpenAction.triggered.connect(self.openFile)
+
+        fileQuitAction = QAction("&Quit", self)
+        fileQuitAction.setShortcut(QKeySequence.Quit)
+        fileQuitAction.triggered.connect(self.quit)
+
+        fileMenu = self.menuBar().addMenu("&File")
+        fileMenu.addAction(fileOpenAction)
+        fileMenu.addAction(fileQuitAction)
+
+    def openFile(self):
+        print("foo")
+
+    def quit(self):
+        self.close()
 
 def main():
     app = QApplication(sys.argv)
