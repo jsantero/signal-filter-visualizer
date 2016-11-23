92d226aec222343f45dd88897400f589ebef047c
Juha-Matti Santero <juha.santero@gmail.com>
2016-11-23 16:10:38 +0200

    Added some icons for toolbar and menu.

---
+++ icons/__init__.py
@@ -0,0 +1,6 @@
+"""
+filter.png from https://icons8.com/web-app/32614/Filtration
+graph.png from https://icons8.com/web-app/9516/Heart-Monitor-Filled
+sine.png from https://icons8.com/web-app/11743/Sine
+
+"""
